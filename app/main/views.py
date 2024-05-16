8573d8f1eeba41948eb7033ade9038bf45224b71
Dmitry Varchak <dmitvar@gmail.com>
2024-05-16 22:35:48 +0300

    The main app is ready, pages index and about

--- app/main/views.py
+++ app/main/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Главная страница магазина - HOME",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": True,
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
     }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "HOME - О нас",
+        "content": "О нас",
+        "text_on_page": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    }
+    return render(request, "main/about.html", context)
