93d724abd8fabc2bdeaf233aa9335b706441c703
Dmitry Varchak <dmitvar@gmail.com>
2024-05-17 21:16:40 +0300

    add shipping.html, views.shipping, urls_shipping

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, about
+from .views import index, about, shipping
 
 app_name = "main"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("shipping/", shipping, name="shipping"),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import context
 
 
 def index(request):
@@ -17,3 +18,28 @@ def about(request):
         "text_on_page": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }
     return render(request, "main/about.html", context)
+
+
+def shipping(request):
+    context = {
+        "title": "HOME - Доставка",
+        "content": "Доставка",
+        "text_on_page": [
+            {
+                "title": "Железнодорожный",
+                "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                "price": f"стоимость: {15.60}$",
+            },
+            {
+                "title": "Морской",
+                "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                "price": f"стоимость: {15.60}$",
+            },
+            {
+                "title": "Авиа",
+                "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                "price": f"стоимость: {15.60}$",
+            },
+        ],
+    }
+    return render(request, "main/shipping.html", context)
